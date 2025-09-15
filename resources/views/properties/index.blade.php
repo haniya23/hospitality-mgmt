4e239bc766c3e8486df648d598c22ba7d6ade3a1
Muhammed Niyas K M <mhdniays@icloud.com>
2025-09-15 15:57:43 +0530

    Update index.blade.php

--- resources/views/properties/index.blade.php
+++ resources/views/properties/index.blade.php
@@ -36,12 +36,18 @@
                         @php
                             $mainImage = $property->photos()->where('is_main', true)->first();
                         @endphp
-                        @if($mainImage)
+                        @if($mainImage && file_exists(public_path($mainImage->file_path)))
                             <div class="relative h-32 sm:h-48 w-full">
                                 <img src="{{ asset($mainImage->file_path) }}" 
                                      alt="{{ $property->name }}" 
                                      class="w-full h-full object-cover"
                                 >
+                            </div>
+                        @elseif($mainImage)
+                            <div class="relative h-32 sm:h-48 w-full bg-gray-200 flex items-center justify-center">
+                                <svg class="h-12 w-12 text-gray-400" fill="none" stroke="currentColor" viewBox="0 0 24 24">
+                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 16l4.586-4.586a2 2 0 012.828 0L16 16m-2-2l1.586-1.586a2 2 0 012.828 0L20 14m-6-6h.01M6 20h12a2 2 0 002-2V6a2 2 0 00-2-2H6a2 2 0 00-2 2v12a2 2 0 002 2z"></path>
+                                </svg>
                             </div>
                         @endif
                         <div class="p-4 sm:p-6">
